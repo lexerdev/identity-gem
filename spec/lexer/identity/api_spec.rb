52d749e5db5b15c458e949cb5b6565c18271eebd
d2kagw <aaron.wallis@gmail.com>
2015-06-07 16:59:34 +1000

    code formatting

--- spec/lexer/identity/api_spec.rb
+++ spec/lexer/identity/api_spec.rb
@@ -12,18 +12,18 @@ describe Lexer::Identity do
     end
     it 'produces a valid request' do
       stub_request(:post, 'https://identity.lexer.io/identity').
-        with(body:'{"links":{"email":["user1@brand.com","usera@brand.com"],"mobile":"61440000000"},"attributes":{"com.brand.car":"Tesla","com.brand.code":10,"com.brand.products":["a","b","c"],"com.brand.detail":{"make":"cake"}},"api_token":"abc-123","contributor_token":"bcd-234"}', headers:{ 'Content-Type'=>'application/json' }).
-        to_return(status:200, body:'{"id":"0a224111-ac64-4142-9198-adf8bf2c1a04"}')
+        with(body: '{"links":{"email":["user1@brand.com","usera@brand.com"],"mobile":"61440000000"},"attributes":{"com.brand.car":"Tesla","com.brand.code":10,"com.brand.products":["a","b","c"],"com.brand.detail":{"make":"cake"}},"api_token":"abc-123","contributor_token":"bcd-234"}', headers: { 'Content-Type' => 'application/json' }).
+        to_return(status: 200, body: '{"id":"0a224111-ac64-4142-9198-adf8bf2c1a04"}')
 
       Lexer::Identity.enrich(
         links: {
-          email:%w(user1@brand.com usera@brand.com),
-          mobile:'61440000000'
-        }, attributes:{
-          'com.brand.car':'Tesla',
-          'com.brand.code':10,
-          'com.brand.products':%w(a b c),
-          'com.brand.detail':{ make: 'cake' }
+          email: %w(user1@brand.com usera@brand.com),
+          mobile: '61440000000'
+        }, attributes: {
+          'com.brand.car' => 'Tesla',
+          'com.brand.code' => 10,
+          'com.brand.products' => %w(a b c),
+          'com.brand.detail' => { make: 'cake' }
         }
       )
 
@@ -31,14 +31,14 @@ describe Lexer::Identity do
     end
     it 'returns an EnrichedResult' do
       stub_request(:post, 'https://identity.lexer.io/identity').
-        with(body:'{"links":{"email":"user1@brand.com"},"attributes":{"com.brand.car":"Tesla"},"api_token":"abc-123","contributor_token":"bcd-234"}', headers:{ 'Content-Type'=>'application/json' }).
-        to_return(status:200, body:'{"id":"0a224111-ac64-4142-9198-adf8bf2c1a04"}')
+        with(body: '{"links":{"email":"user1@brand.com"},"attributes":{"com.brand.car":"Tesla"},"api_token":"abc-123","contributor_token":"bcd-234"}', headers: { 'Content-Type' => 'application/json' }).
+        to_return(status: 200, body: '{"id":"0a224111-ac64-4142-9198-adf8bf2c1a04"}')
 
       result = Lexer::Identity.enrich(
-        links:{
-          email:'user1@brand.com'
-        }, attributes:{
-          'com.brand.car':'Tesla'
+        links: {
+          email: 'user1@brand.com'
+        }, attributes: {
+          'com.brand.car' => 'Tesla'
         }
       )
 
@@ -58,37 +58,37 @@ describe Lexer::Identity do
     end
     it 'produces a valid request' do
       stub_request(:post, 'https://identity.lexer.io/identity').
-        with(body:'{"links":{"email":["user1@brand.com","usera@brand.com"],"mobile":"61440000000"},"api_token":"abc-123","consumer_token":"bcd-234"}', headers:{ 'Content-Type'=>'application/json' }).
-        to_return(status:200, body:'{"id":"0a224111-ac64-4142-9198-adf8bf2c1a04","attributes":{"com.brand.car":"Tesla","com.brand.code":10,"com.brand.products":["a","b","c"],"com.brand.detail":{"make":"cake"}}}')
+        with(body: '{"links":{"email":["user1@brand.com","usera@brand.com"],"mobile":"61440000000"},"api_token":"abc-123","consumer_token":"bcd-234"}', headers: { 'Content-Type' => 'application/json' }).
+        to_return(status: 200, body: '{"id":"0a224111-ac64-4142-9198-adf8bf2c1a04","attributes":{"com.brand.car":"Tesla","com.brand.code":10,"com.brand.products":["a","b","c"],"com.brand.detail":{"make":"cake"}}}')
 
       Lexer::Identity.enrich(
-        links:{
-          email:%w(user1@brand.com usera@brand.com),
-          mobile:'61440000000'
-        }, attributes:{
-          'com.brand.car':'Tesla' # note: this will be discarded as consumers can't contribute
+        links: {
+          email: %w(user1@brand.com usera@brand.com),
+          mobile: '61440000000'
+        }, attributes: {
+          'com.brand.car' => 'Tesla' # note: this will be discarded as consumers can't contribute
         }
       )
 
-      assert_requested(:post, 'https://identity.lexer.io/identity', times:1)
+      assert_requested(:post, 'https://identity.lexer.io/identity', times: 1)
     end
     it 'returns an EnrichedResult' do
       stub_request(:post, 'https://identity.lexer.io/identity').
-        with(body:'{"links":{"email":["user1@brand.com","usera@brand.com"],"mobile":"61440000000"},"api_token":"abc-123","consumer_token":"bcd-234"}', headers:{ 'Content-Type'=>'application/json' }).
-        to_return(status:200, body:'{"id":"0a224111-ac64-4142-9198-adf8bf2c1a04","attributes":{"com.brand.car":"Tesla","com.brand.code":10,"com.brand.products":["a","b","c"],"com.brand.detail":{"make":"cake"}}}')
+        with(body: '{"links":{"email":["user1@brand.com","usera@brand.com"],"mobile":"61440000000"},"api_token":"abc-123","consumer_token":"bcd-234"}', headers: { 'Content-Type' => 'application/json' }).
+        to_return(status: 200, body: '{"id":"0a224111-ac64-4142-9198-adf8bf2c1a04","attributes":{"com.brand.car":"Tesla","com.brand.code":10,"com.brand.products":["a","b","c"],"com.brand.detail":{"make":"cake"}}}')
 
       result = Lexer::Identity.enrich(
-        links:{
-          email:%w(user1@brand.com usera@brand.com),
-          mobile:'61440000000'
-        }, attributes:{
-          'com.brand.car':'Tesla' # note: this will be discarded as consumers can't contribute
+        links: {
+          email: %w(user1@brand.com usera@brand.com),
+          mobile: '61440000000'
+        }, attributes: {
+          'com.brand.car' => 'Tesla' # note: this will be discarded as consumers can't contribute
         }
       )
 
       result.must_be_instance_of Lexer::Identity::EnrichedResult
       result.id.must_be_kind_of String
-      hash = { 'com.brand.car'=>'Tesla', 'com.brand.code'=>10, 'com.brand.products'=>%w(a b c), 'com.brand.detail'=>{ 'make'=>'cake' } }
+      hash = { 'com.brand.car' => 'Tesla', 'com.brand.code' => 10, 'com.brand.products' => %w(a b c), 'com.brand.detail' => { 'make' => 'cake' } }
       result.attributes.must_equal hash
     end
   end
